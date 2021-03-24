ca3347f3f09dbab5bcc01f52d7e7a4c4eb5afce7
Miria Santos <miriaandressa1@gmail.com>
2021-03-24 10:39:15 -0300

    Models, Controller, Reposiroty

---
+++ farmacia/farmacia/src/main/java/org/generation/farmacia/model/Categoria.java
@@ -0,0 +1,60 @@
+package org.generation.farmacia.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 20)
+	private String marca;
+	
+	@NotNull
+	@Size(min = 5, max = 20)
+	private String setor;
+	
+	@OneToMany
+	@JsonIgnoreProperties("produto")
+	private List<Categoria> produto;
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+}
